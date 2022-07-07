09e8a3af687ca0d96cb1ba8593414e88d009bb51
Jonathan Calderon <jonathanalosha@gmail.com>
2022-07-06 19:45:06 -0500

    adding backend

---
+++ models/posts.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema({
+  base: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  upVotes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  downVotes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  datecreated: {
+    type: Date, 
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Issue', issueSchema);
