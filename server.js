54daba8d2343647abefd663ff97d57851a84930e
Jonathan Calderon <jonathanalosha@gmail.com>
2022-07-09 13:07:53 -0500

    deploy

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`[+] Starting server on port ${PORT}`)
 })
 
